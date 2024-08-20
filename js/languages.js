dc7a135b4b82636eca7942f4ebe3fb2e2dc83f0d
Hennry Chan <hennry.chan@blueoceantech.com.mx>
2024-08-20 12:34:31 -0600

    Correcciones y ajustes del menú de idiomas

--- js/languages.js
+++ js/languages.js
@@ -1,18 +1,23 @@
+/* Se encarga del cambio de idioma y del desplegue del menú */
+
 const languages = document.getElementById('lenguage');
 const listLanguage = document.getElementById('lenguages');
+const langButtons = document.querySelectorAll("[data-language]");
+const textsToChange = document.querySelectorAll("[data-section]");
 
 // Toggle lista idiomas
 languages.addEventListener('click',()=>{
     listLanguage.classList.toggle('toggle');
 });
 
-
-
-const langButtons = document.querySelectorAll("[data-language]");
-const textsToChange = document.querySelectorAll("[data-section]");
-
+//Realiza el cambio de idioma
 langButtons.forEach((button) => {
     button.addEventListener("click", () => {
+
+        //Realiza el efecto de ocultar el menú despues del clic
+        listLanguage.classList.toggle('toggle');        
+        
+        //Realiza el reoorrido y remplaza el idioma
         fetch(`../languages/${button.dataset.language}.json`)
             .then(res => res.json())
             .then(data => {
